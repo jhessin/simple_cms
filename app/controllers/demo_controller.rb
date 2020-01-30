ab28456f938c92452465461251633ec880114647
Jim Hessin <jhessin@gmail.com>
2020-01-29 21:42:43 -0700

    finished chapter 3

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,5 +3,21 @@ class DemoController < ApplicationController
   layout false
 
   def index
+    render 'index'
+  end
+
+  def hello
+    @array = [*(1..5)]
+    @id = params[:id].to_i
+    @page = params[:page].to_i
+    render 'hello'
+  end
+
+  def other_hello
+    redirect_to( action: 'hello' )
+  end
+
+  def lynda
+    redirect_to('http://lynda.com')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   get 'home/index'
-  root 'demo#index'
 
   get 'demo/index'
   # ^ is equivalent to v
   # match 'demo/index', :to => 'demo#index', :via => :get
+  get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/lynda'
 
   # default route
   # may go away in future versions of rails
-  get ':controller(/:action(/:id))'
+  # get ':controller(/:action(/:id))'
 
   # For details on the DSL available within this file, see
   # https://guides.rubyonrails.org/routing.html
