29c15269b4cc83ac812eba8cc86be3239635ab10
Jim Hessin <jhessin@gmail.com>
2020-01-29 20:27:41 -0700

    finished chapter 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  root 'demo#index'
+
+  get 'demo/index'
+  # ^ is equivalent to v
+  # match 'demo/index', :to => 'demo#index', :via => :get
+
+  # default route
+  # may go away in future versions of rails
+  get ':controller(/:action(/:id))'
+
+  # For details on the DSL available within this file, see
+  # https://guides.rubyonrails.org/routing.html
 end
