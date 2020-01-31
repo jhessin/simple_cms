0f2afda619847cb63835a1e57d63897789d954b3
Jim Hessin <jhessin@gmail.com>
2020-01-30 18:17:28 -0700

    Completed chapter 5

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,30 @@
+# frozen_string_literal: true
+
 class Subject < ApplicationRecord
+  # scope :visible, -> { where(visible: true) }
+  # scope :invisible, -> { where(visible: false) }
+  # scope :sorted, -> { order('position ASC') }
+  # scope :newest_first, -> { order('created_at DESC') }
+  # scope :search, lamda { |query| where(['name LIKE ?', "%#{query}%"]) }
+  class << self
+    def visible
+      where(visible: true)
+    end
+
+    def invisible
+      where(visible: false)
+    end
+
+    def sorted
+      order(:position)
+    end
+
+    def newest_first
+      order(created_at: :desc)
+    end
+
+    def search(query)
+      where(['name LIKE ?', "%#{query}%"])
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# Hold information pertaining to a user
 class User < ApplicationRecord
+  # @type [Symbol]
+  self.table_name = :admin_users
 end
