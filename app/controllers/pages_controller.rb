fb8b4478b887bf9dc2aef3439b694a2f99d2d7b0
Jim Hessin <jhessin@gmail.com>
2020-02-07 17:04:09 -0700

    copy views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,15 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
   def index
+    @pages = Page.all
   end
 
-  def delete
-  end
+  def delete; end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
-  def new
-  end
+  def new; end
 end
