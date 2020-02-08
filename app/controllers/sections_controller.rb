c4d49c3d63e80973b53a4172ece4ed6aa5947038
Jim Hessin <jhessin@gmail.com>
2020-02-08 12:19:50 -0700

    finished sections

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,8 +5,20 @@ class SectionsController < ApplicationController
     @sections = Section.all
   end
 
-  def delete
-    @section = Section.find params[:id]
+  def new
+    @section = Section.new
+  end
+
+  def create
+    @section = Section.new section_params
+
+    if @section.save
+      flash[:notice] = 'Section created successfully'
+      redirect_to section_path(@section)
+    else
+      flash[:error] = 'Error creating section'
+      render 'new'
+    end
   end
 
   def show
@@ -17,7 +29,34 @@ class SectionsController < ApplicationController
     @section = Section.find params[:id]
   end
 
-  def new
+  def update
     @section = Section.find params[:id]
+
+    if @section.update section_params
+      flash[:notice] = 'Section updated successfully'
+      redirect_to section_path(@section)
+    else
+      flash[:error] = 'Error updating section'
+      render 'edit'
+    end
+  end
+
+  def delete
+    @section = Section.find params[:id]
+  end
+
+  def destroy
+    @section = Section.find params[:id]
+
+    @section.destroy
+    flash[:notice] = "Section '#{@section.name}' deleted successfully"
+    redirect_to sections_path
+  end
+
+  private
+
+  def section_params
+    params.require(:section).permit(:name, :page_id, :position, :visible,
+                                    :content_type, :content)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,67 +12,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_203105) do
-
+ActiveRecord::Schema.define(version: 20_200_131_203_105) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "admin_users", force: :cascade do |t|
-    t.string "first_name", limit: 25
-    t.string "last_name", limit: 50
-    t.string "email", limit: 100, default: "", null: false
-    t.string "hashed_password", limit: 40
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "username", limit: 25
-    t.index ["username"], name: "index_admin_users_on_username"
+  create_table 'admin_users', force: :cascade do |t|
+    t.string 'first_name', limit: 25
+    t.string 'last_name', limit: 50
+    t.string 'email', limit: 100, default: '', null: false
+    t.string 'hashed_password', limit: 40
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'username', limit: 25
+    t.index ['username'], name: 'index_admin_users_on_username'
   end
 
-  create_table "admin_users_pages", id: false, force: :cascade do |t|
-    t.integer "admin_user_id"
-    t.integer "page_id"
-    t.index ["admin_user_id", "page_id"], name: "index_admin_users_pages_on_admin_user_id_and_page_id"
+  create_table 'admin_users_pages', id: false, force: :cascade do |t|
+    t.integer 'admin_user_id'
+    t.integer 'page_id'
+    t.index %w[admin_user_id page_id], name: 'index_admin_users_pages_on_admin_user_id_and_page_id'
   end
 
-  create_table "pages", force: :cascade do |t|
-    t.integer "subject_id"
-    t.string "name"
-    t.integer "permalink"
-    t.integer "position"
-    t.boolean "visible", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["permalink"], name: "index_pages_on_permalink"
-    t.index ["subject_id"], name: "index_pages_on_subject_id"
+  create_table 'pages', force: :cascade do |t|
+    t.integer 'subject_id'
+    t.string 'name'
+    t.integer 'permalink'
+    t.integer 'position'
+    t.boolean 'visible', default: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['permalink'], name: 'index_pages_on_permalink'
+    t.index ['subject_id'], name: 'index_pages_on_subject_id'
   end
 
-  create_table "section_edits", force: :cascade do |t|
-    t.integer "admin_user_id"
-    t.integer "section_id"
-    t.string "summary"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["admin_user_id", "section_id"], name: "index_section_edits_on_admin_user_id_and_section_id"
+  create_table 'section_edits', force: :cascade do |t|
+    t.integer 'admin_user_id'
+    t.integer 'section_id'
+    t.string 'summary'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index %w[admin_user_id section_id], name: 'index_section_edits_on_admin_user_id_and_section_id'
   end
 
-  create_table "sections", force: :cascade do |t|
-    t.integer "page_id"
-    t.string "name"
-    t.integer "position"
-    t.boolean "visible", default: false
-    t.string "content_type"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["page_id"], name: "index_sections_on_page_id"
+  create_table 'sections', force: :cascade do |t|
+    t.integer 'page_id'
+    t.string 'name'
+    t.integer 'position'
+    t.boolean 'visible', default: false
+    t.string 'content_type'
+    t.text 'content'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['page_id'], name: 'index_sections_on_page_id'
   end
 
-  create_table "subjects", force: :cascade do |t|
-    t.string "name"
-    t.integer "position"
-    t.boolean "visible", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'subjects', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'position'
+    t.boolean 'visible', default: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
