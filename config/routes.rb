a318185bcb23d2d00dd43aad61b7cec80c4e6b1c
Jim Hessin <jhessin@gmail.com>
2020-02-13 20:19:18 -0700

    removed unused code

--- config/routes.rb
+++ config/routes.rb
@@ -37,19 +37,6 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'home/index'
-  get 'demo/index'
-  # ^ is equivalent to v
-  # match 'demo/index', :to => 'demo#index', :via => :get
-  get 'demo/hello'
-  get 'demo/other_hello'
-  get 'demo/lynda'
-  get 'demo/escape_output'
-
-  # default route
-  # may go away in future versions of rails
-  # get ':controller(/:action(/:id))'
-
   # For details on the DSL available within this file, see
   # https://guides.rubyonrails.org/routing.html
 end
